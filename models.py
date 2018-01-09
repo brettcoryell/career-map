6955b0003f2e039bccf472bbc5ed593a78e71eb5
Akarsh Ghanathay <akarshg@gmail.com>
2018-01-09 11:12:13 -0600

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from wtforms import Form, StringField, validators
-
-class InputForm(Form):
-    A = StringField(
-        label='Start Position', default='Computer Operations Specialist 1',
-        validators=[validators.InputRequired()])
-    B = StringField(
-        label='End Position', default='Tech Title 3',
-        validators=[validators.InputRequired()])
